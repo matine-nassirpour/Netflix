a1198926acd75bd6566c155d6367e43b33b7ba42
MaddieDeveloper <matine.nassirpour@maddie.doctor>
2022-03-09 14:44:01 +0100

    fix class name

--- fontwebservice/src/main/java/com/example/fontwebservice/FrontWebServiceApplication.java
+++ fontwebservice/src/main/java/com/example/fontwebservice/FrontWebServiceApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FontwebserviceApplication {
+public class FrontWebServiceApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(FontwebserviceApplication.class, args);
+		SpringApplication.run(FrontWebServiceApplication.class, args);
 	}
-
 }
