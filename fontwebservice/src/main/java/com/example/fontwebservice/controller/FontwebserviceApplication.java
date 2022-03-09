5b2cffebe187167a327f53dc49379351e1250be5
Bryan Dane <kwuimobryan@gmail.com>
2022-03-09 15:04:55 +0100

    Update FontwebserviceApplication.java

--- fontwebservice/src/main/java/com/example/fontwebservice/controller/FontwebserviceApplication.java
+++ fontwebservice/src/main/java/com/example/fontwebservice/controller/FontwebserviceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
+@RestController
 public class FontwebserviceApplication {
     @Autowired
     DiscoveryClient discoveryClient;
@@ -17,6 +19,7 @@ public class FontwebserviceApplication {
         ServiceInstance test = instances.get(0);
         String hostname = test.getHost();
         int port = test.getPort();
+
         RestTemplate restTemplate = new RestTemplate();
         String microservice1Address = "http://" + hostname + ":" + port;
         ResponseEntity<String> response =
