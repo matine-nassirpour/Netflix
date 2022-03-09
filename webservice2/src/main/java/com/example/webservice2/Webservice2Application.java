343674994b070149dd44aae7319b8c0338967c73
Bryan Dane <kwuimobryan@gmail.com>
2022-03-09 16:49:04 +0100

    update web service 2

---
+++ webservice2/src/main/java/com/example/webservice2/Webservice2Application.java
@@ -0,0 +1,16 @@
+package com.example.webservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+public class Webservice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webservice2Application.class, args);
+	}
+
+
+}
