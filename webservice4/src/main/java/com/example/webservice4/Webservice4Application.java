72e6d40c620a2e237267f7e73267c0e7e630754c
KWUIMO KWEKAM Bryan Dane <kwuimobryan@gmail.com>
2022-05-10 11:04:27 +0200

    add zuul

---
+++ webservice4/src/main/java/com/example/webservice4/Webservice4Application.java
@@ -0,0 +1,13 @@
+package com.example.webservice4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webservice4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webservice4Application.class, args);
+	}
+
+}
